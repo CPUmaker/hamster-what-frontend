e28cfb3a17656207f7ded8f4d62ee59dad8dce14
Chris Li <c58li@uwaterloo.ca>
2023-01-24 13:13:25 -0500

    bug exists

--- src/screens/RegisterScreen.js
+++ src/screens/RegisterScreen.js
@@ -8,7 +8,8 @@ import {
   TouchableOpacity,
 } from "react-native";
 import axios from "axios";
-// import { useToast } from 'native-base';
+import { Formik } from "formik";
+import * as Yup from "yup";
 import { AntDesign, MaterialIcons } from "@expo/vector-icons";
 
 import RegisterSVG from "../../assets/misc/register.svg";
@@ -17,136 +18,185 @@ import AppleSVG from "../../assets/misc/apple.svg";
 import TwitterSVG from "../../assets/misc/twitter.svg";
 import { BASE_URL } from "../config";
 
-// const toast = useToast();
+const SignUpSchema = Yup.object().shape({
+  username: Yup.string()
+    .min(6, "Must be 6 characters or more.")
+    .max(20, "Must be 20 characters or less.")
+    .required("Please enter your preferred name."),
+  email: Yup.string()
+    .email("Invalid email address")
+    .required("Please enter your email address."),
+  password: Yup.string()
+    .min(8, "Must be 8 characters or more.")
+    .matches(
+      /^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,}$/,
+      "Must contain 8 characters, at least one uppercase letter, one lowercase letter, one numeric character, and one special character."
+    )
+    .required("Please enter your password."),
+  confirmPassword: Yup.string()
+    .min(8, "Must be 8 characters or more.")
+    .oneOf(Yup.ref("password"), "Your passwords do not match")
+    .required("Confirm password is required."),
+});
 
 export default function RegisterScreen({ navigation }) {
-  const [email, setEmail] = useState(null);
-  const [username, setUsername] = useState(null);
-  const [password, setPassword] = useState(null);
-  const [confirmPassword, setConfirmPassword] = useState(null);
-
-  const register = (email, username, password, confirmPassword) => {
-    if (password !== confirmPassword) {
-      console.log("Inconsistent of password");
-      // toast.show({title: 'Inconsistent of password', placement: 'top'});
-    } else {
-      axios
-        .post(`${BASE_URL}/api/auth/register`, { email, username, password })
-        .then((res) => {
-          console.log(res.data);
-        })
-        .catch((error) => {
-          console.log(JSON.stringify(error.response.data));
-          // toast.show({title: error.response.data.username, placement: 'top'});
-        });
-    }
-  };
-
   return (
-    <View style={styles.container}>
-      <ScrollView
-        showsVerticalScrollIndicator={false}
-        style={{ paddingHorizontal: 25 }}
-      >
-        <View style={styles.svg_container}>
-          <RegisterSVG height={300} width={300} />
+    <Formik
+      initialValues={{
+        email: "",
+        username: "",
+        password: "",
+        confirmPassword: "",
+      }}
+      validationSchema={SignUpSchema}
+      onSubmit={(values) => {
+        axios
+          .post(`${BASE_URL}/api/auth/register`, {
+            email: values.email,
+            username: values.username,
+            password: values.password,
+          })
+          .then((res) => {
+            console.log(res.data);
+          })
+          .catch((error) => {
+            console.log(JSON.stringify(error.response.data));
+            // toast.show({title: error.response.data.username, placement: 'top'});
+          });
+      }}
+    >
+      {({
+        values,
+        errors,
+        touched,
+        handleChange,
+        setFieldTouched,
+        isValid,
+        handleSubmit,
+      }) => (
+        <View style={styles.container}>
+          <ScrollView
+            showsVerticalScrollIndicator={false}
+            style={{ paddingHorizontal: 25 }}
+          >
+            <View style={styles.svg_container}>
+              <RegisterSVG height={300} width={300} />
+            </View>
+
+            <Text style={styles.font}>Register</Text>
+
+            <View style={styles.text_input_container}>
+              <AntDesign
+                name="user"
+                size={20}
+                color="#666"
+                style={{ marginRight: 5 }}
+              />
+              <TextInput
+                placeholder="Username"
+                autoCapitalize="none"
+                style={styles.text_input}
+                value={values.username}
+                onChangeText={handleChange("username")}
+                onBlur={() => setFieldTouched("username")}
+              />
+              {touched.username && errors.username && (
+                <Text style={styles.error_text}>{errors.username}</Text>
+              )}
+            </View>
+
+            <View style={styles.text_input_container}>
+              <MaterialIcons
+                name="alternate-email"
+                size={20}
+                color="#666"
+                style={{ marginRight: 5 }}
+              />
+              <TextInput
+                placeholder="Email"
+                autoCapitalize="none"
+                style={styles.text_input}
+                value={values.email}
+                onChangeText={handleChange("email")}
+              />
+            </View>
+
+            <View style={styles.text_input_container}>
+              <AntDesign
+                name="lock"
+                size={20}
+                color="#666"
+                style={{ marginRight: 5 }}
+              />
+              <TextInput
+                placeholder="Password"
+                style={styles.text_input}
+                secureTextEntry={true}
+                value={values.password}
+                onChangeText={handleChange("password")}
+              />
+            </View>
+
+            <View style={styles.text_input_container}>
+              <AntDesign
+                name="lock"
+                size={20}
+                color="#666"
+                style={{ marginRight: 5 }}
+              />
+              <TextInput
+                placeholder="Confirm Password"
+                style={styles.text_input}
+                secureTextEntry={true}
+                value={values.confirmPassword}
+                onChangeText={(text) => handleChange("confirmPassword")}
+              />
+            </View>
+
+            <TouchableOpacity
+              style={[
+                styles.login_button,
+                { backgroundColor: isValid ? "#AD40AF" : "#A5C9CA" },
+              ]}
+              onPress={handleSubmit}
+              disabled={!isValid}
+            >
+              <Text style={styles.login_text}>Register</Text>
+            </TouchableOpacity>
+
+            <Text style={styles.alter_login_text}>Or, register with ...</Text>
+
+            <View style={styles.alter_login_container}>
+              <TouchableOpacity
+                style={styles.alter_login_icon}
+                onPress={() => {}}
+              >
+                <GoogleSVG height={24} width={24} />
+              </TouchableOpacity>
+              <TouchableOpacity
+                style={styles.alter_login_icon}
+                onPress={() => {}}
+              >
+                <AppleSVG height={24} width={24} />
+              </TouchableOpacity>
+              <TouchableOpacity
+                style={styles.alter_login_icon}
+                onPress={() => {}}
+              >
+                <TwitterSVG height={24} width={24} />
+              </TouchableOpacity>
+            </View>
+
+            <View style={styles.register_container}>
+              <Text>Already registered?</Text>
+              <TouchableOpacity onPress={() => navigation.goBack()}>
+                <Text style={styles.register_text}> Login</Text>
+              </TouchableOpacity>
+            </View>
+          </ScrollView>
         </View>
-
-        <Text style={styles.font}>Register</Text>
-
-        <View style={styles.text_input_container}>
-          <AntDesign
-            name="user"
-            size={20}
-            color="#666"
-            style={{ marginRight: 5 }}
-          />
-          <TextInput
-            placeholder="Username"
-            autoCapitalize="none"
-            style={styles.text_input}
-            value={username}
-            onChangeText={(text) => setUsername(text)}
-          />
-        </View>
-
-        <View style={styles.text_input_container}>
-          <MaterialIcons
-            name="alternate-email"
-            size={20}
-            color="#666"
-            style={{ marginRight: 5 }}
-          />
-          <TextInput
-            placeholder="Email"
-            autoCapitalize="none"
-            style={styles.text_input}
-            value={email}
-            onChangeText={(text) => setEmail(text)}
-          />
-        </View>
-
-        <View style={styles.text_input_container}>
-          <AntDesign
-            name="lock"
-            size={20}
-            color="#666"
-            style={{ marginRight: 5 }}
-          />
-          <TextInput
-            placeholder="Password"
-            style={styles.text_input}
-            secureTextEntry={true}
-            value={password}
-            onChangeText={(text) => setPassword(text)}
-          />
-        </View>
-
-        <View style={styles.text_input_container}>
-          <AntDesign
-            name="lock"
-            size={20}
-            color="#666"
-            style={{ marginRight: 5 }}
-          />
-          <TextInput
-            placeholder="Confirm Password"
-            style={styles.text_input}
-            secureTextEntry={true}
-            value={confirmPassword}
-            onChangeText={(text) => setConfirmPassword(text)}
-          />
-        </View>
-
-        <TouchableOpacity
-          style={styles.login_button}
-          onPress={() => register(email, username, password, confirmPassword)}
-        >
-          <Text style={styles.login_text}>Register</Text>
-        </TouchableOpacity>
-
-        <Text style={styles.alter_login_text}>Or, register with ...</Text>
-
-        <View style={styles.alter_login_container}>
-          <TouchableOpacity style={styles.alter_login_icon} onPress={() => {}}>
-            <GoogleSVG height={24} width={24} />
-          </TouchableOpacity>
-          <TouchableOpacity style={styles.alter_login_icon} onPress={() => {}}>
-            <AppleSVG height={24} width={24} />
-          </TouchableOpacity>
-          <TouchableOpacity style={styles.alter_login_icon} onPress={() => {}}>
-            <TwitterSVG height={24} width={24} />
-          </TouchableOpacity>
-        </View>
-
-        <View style={styles.register_container}>
-          <Text>Already registered?</Text>
-          <TouchableOpacity onPress={() => navigation.goBack()}>
-            <Text style={styles.register_text}> Login</Text>
-          </TouchableOpacity>
-        </View>
-      </ScrollView>
-    </View>
+      )}
+    </Formik>
   );
 }
 
@@ -218,5 +268,9 @@ const styles = StyleSheet.create({
   register_text: {
     color: "#AD40AF",
     fontWeight: "700",
+  },
+  error_text: {
+    fontSize: 10,
+    color: "#FF0D10",
   },
 });
