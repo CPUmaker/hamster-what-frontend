8d4ec247963fd5f8440b414fa9b06de4db39888a
Xuhua Luo (Evan) <x68luo@uwaterloo.ca>
2023-03-24 14:25:02 -0400

    fix config.js

--- src/config.js
+++ src/config.js
@@ -1,18 +1,18 @@
-export const BASE_URL = 'http://127.0.0.1:8000';
+export const BASE_URL = "http://127.0.0.1:8000";
 
 const API_URL = BASE_URL + "/api";
 
 export const endpoints = {
-    register: API_URL + "/auth/register",
-    login: API_URL + "/auth/login",
-    logout: API_URL + "/auth/logout",
-    validate_token: API_URL + "/auth/validate-token",
-    apple: API_URL + "/auth/apple",
-    google: API_URL + "/auth/google",
-    bill: API_URL + "/bill/",
-    facebook: API_URL + "/auth/facebook",
-    profile: API_URL + "/profile",
-    search: API_URL + "/bill/search/",
-    pricesum: API_URL + "/bill/price-sum/",,
-    coupon: API_URL + "/coupon",
-}
+  register: API_URL + "/auth/register",
+  login: API_URL + "/auth/login",
+  logout: API_URL + "/auth/logout",
+  validate_token: API_URL + "/auth/validate-token",
+  apple: API_URL + "/auth/apple",
+  google: API_URL + "/auth/google",
+  bill: API_URL + "/bill/",
+  facebook: API_URL + "/auth/facebook",
+  profile: API_URL + "/profile",
+  search: API_URL + "/bill/search/",
+  pricesum: API_URL + "/bill/price-sum/",
+  coupon: API_URL + "/coupon",
+};
