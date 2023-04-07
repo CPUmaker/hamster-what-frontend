65600ccbf0431146655ff1792635d96543d36ba5
Yazi Chen <y237chen@uwaterloo.ca>
2023-04-06 20:00:22 -0400

    change list icon

--- src/components/ListItem.js
+++ src/components/ListItem.js
@@ -2,15 +2,102 @@ import React from "react";
 import { StyleSheet, View, Text, TouchableOpacity } from "react-native";
 import { AntDesign } from "@expo/vector-icons";
 
+import {
+  Ionicons,
+  FontAwesome5,
+  MaterialCommunityIcons,
+  MaterialIcons,
+  Entypo,
+} from "@expo/vector-icons";
+
+const categories = [
+  { id: 1, 
+    name: "Food", 
+    icon: <Ionicons name="fast-food-outline" size={30} color={"#0aada8"} />
+  },
+  { id: 2, 
+    name: "Transportation", 
+    icon:  <Ionicons name = "bus-outline" size={30} color = {"#0aada8" } />
+  },
+  { id: 3, 
+    name: "Shopping", 
+    icon:  <Ionicons name = "cart-outline" size={30} color = {"#0aada8"} />
+  },
+  {
+    id: 4,
+    name: "Entertainment",
+    icon:  <Ionicons name = "game-controller-outline" size={30} color = {"#0aada8"} />
+  },
+  { id: 5, 
+    name: "Housing", 
+    icon:  <Ionicons name = "home-outline" size={30} color = {"#0aada8"} />
+  },
+  { id: 6, 
+    name: "Utilities", 
+    icon:  <Ionicons name = "flash-outline" size={30} color = {"#0aada8"} />
+  },
+  { id: 7, 
+    name: "Other", 
+    icon:  <Ionicons name = "file-tray" size={30} color = {"#0aada8" } />
+  },
+  {
+    id: 8,
+    name: "Salary",
+    icon: (
+      <FontAwesome5 name="hand-holding-usd" size={30} color={"#A04AAA"} />
+    ),
+  },
+  {
+    id: 9,
+    name: "Interest",
+    icon: (
+      <MaterialCommunityIcons name="bank-outline" size={30} color={"#A04AAA"} />
+    ),
+  },
+  {
+    id: 10,
+    name: "Investments",
+    icon: (
+      <MaterialIcons name="attach-money" size={30} color={"#A04AAA"} />
+    ),
+  },
+  {
+    id: 11,
+    name: "Child benefit",
+    icon: (
+      <MaterialCommunityIcons name="baby-face-outline" size={30} color={"#A04AAA"} />
+    ),
+  },
+  {
+    id: 12,
+    name: "Pension",
+    icon: <Entypo name="shield" size={30} color="#A04AAA" />,
+  },
+  {
+    id: 13,
+    name: "Income",
+    icon: (
+      <FontAwesome5 name="money-bill-alt" size={24} color="#A04AAA" />
+    ),
+  },
+  { id: 7, name: "Other", icon: "file-tray", color: "#A04AAA" },
+];
+
+const getCategoryIconByName = (name) => {
+  const category = categories.find((c) => c.name === name);
+  return category ? category.icon : <AntDesign name="shoppingcart" size={32} color="#888" />;
+};
+
 export default function ListItem({
     name,
     money,
     onPressCallback
 }) {
+  const categoryIcon = getCategoryIconByName(name);
   return (
     <View style={styles.container}>
       <View style={styles.item_container}>
-        <AntDesign name="shoppingcart" size={32} color="black" />
+        {categoryIcon}
         <View style={{marginLeft: 10}}>
           <Text style={styles.detail_text}>{name}</Text>
         </View>
